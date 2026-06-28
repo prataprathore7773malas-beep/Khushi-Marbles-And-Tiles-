6594c0961c52a2334f9178149b35a994721d506f
prataprathore7773malas-beep <prataprathore7773malas@gmail.com>
2026-06-28 18:10:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -519,3 +519,26 @@ console.log("Layout Updated");
 /*==========================================
         END OF SCRIPT
 ==========================================*/ 
+//========================
+// MOBILE MENU
+//========================
+
+const menuToggle = document.querySelector(".menu-toggle");
+
+const navLinks = document.querySelector(".nav-links");
+
+menuToggle.addEventListener("click", () => {
+
+    navLinks.classList.toggle("active");
+
+});
+
+document.querySelectorAll(".nav-links a").forEach(link => {
+
+    link.addEventListener("click", () => {
+
+        navLinks.classList.remove("active");
+
+    });
+
+});
